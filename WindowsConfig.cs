8eb154578350b29d02d323eced25583b6c3d68b4
SERBAN EC ANDREI <andyspeed2003@gmail.com>
2024-06-13 15:03:35 +0300

    db

--- WindowsConfig.cs
+++ WindowsConfig.cs
@@ -185,13 +185,24 @@ namespace exam
             }
         }
 
-        // citire din db
+        // citire din db + insert DATABASE 
+        static Random random = new Random();
         public List<Angajat> getAngajati()
         {
             List<Angajat> angajati = new List<Angajat>();
             using (var connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=X:\\facultate\\paw\\subiect5\\db.mdf;Integrated Security=True;Connect Timeout=30"))
             {
                 connection.Open();
+
+
+                using (var command = new SqlCommand("INSERT INTO ANGAJATI(Nume, data_nastere, id_companie) values(@n, @d, @i)", connection))
+                {
+                    command.Parameters.Add("n", "Bob Cat");
+                    command.Parameters.Add("d", DateTime.Parse("05.03.2000"));
+                    command.Parameters.Add("i", random.Next(1, 4));
+                    command.ExecuteNonQuery();
+                }
+
 
                 using (var command = new SqlCommand("Select * from Angajati", connection))
                 {
